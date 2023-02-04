0a05af05a27d16ec80cd3900e78ae885120ae7a3
Tariq Rasheed <xs2tariqrasheed@gmail.com>
2023-02-05 01:59:59 +0500

    added break points

--- components/Hero/Hero.tsx
+++ components/Hero/Hero.tsx
@@ -6,8 +6,8 @@ interface HeroProps {
 
 const Hero: FC<HeroProps> = ({ text }: HeroProps) => {
   return (
-    <div className="bg-primary-blue items-center flex pl-64 py-20 h-52">
-      <p className="text-white  text-4xl font-normal leading-5 ">{text}</p>
+    <div className="bg-primary-blue items-center flex xs:pl-16 md:pl-32 lg:pl-64 xs:py-10 md:py-20">
+      <p className="text-white text-4xl font-normal">{text}</p>
     </div>
   );
 };
